0f3ca434b4f34c9db3759980bc0946de861eb4f9
Denis Balashov <balasovdenis508@gmail.com>
2025-03-04 14:26:23 +0300

    добавил меч

---
+++ internal/weapon.py
@@ -0,0 +1,9 @@
+class Weapon:
+    def __init__(self,name, power):
+        self.name = name
+        self.power = power
+
+
+class Sword(Weapon):
+    def __init__(self, name, power):
+        super().__init__( name, power)
