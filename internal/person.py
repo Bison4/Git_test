248aff5de8b7b4fc277fbb1f8942a062265bc154
Denis Balashov <balasovdenis508@gmail.com>
2025-03-04 17:44:16 +0300

    доработал модель hit

--- internal/person.py
+++ internal/person.py
@@ -9,10 +9,11 @@ class Knight(Person):
     def __init__(self, name, health, armor, weapon):
         super().__init__(name, health, armor, weapon)
     def hit(self,enemy):
-        if (enemy.armor - enemy.weapon.power) < 0:
-            damage = (enemy.armor + enemy.health) - enemy.weapon.power
+        if (enemy.armor - self.weapon.power) < 0:
+            damage = (enemy.armor + enemy.health) - self.weapon.power
         else:
             damage = 0
-
-        print(f'{self.name} атакует {enemy.name} с оружием {self.weapon.name} \n {enemy.name} получает {damage} урона! Осталось здоровья: {enemy.health}, брони: {enemy.armor}'
+        if self.weapon.power >= enemy.armor:
+            enemy.armor = 0
+        print(f'{self.name} атакует {enemy.name} с оружием {self.weapon.name} \n {enemy.name} получает {damage} урона! Осталось здоровья: {enemy.health - damage}, брони: {enemy.armor}'
             )
